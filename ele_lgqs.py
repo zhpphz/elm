090d144ea9411cbabd00d5c488aa8b2da464b6fc
zhpphz <121403179+zhpphz@users.noreply.github.com>
2024-09-14 12:05:22 +0800

    Update ele_lgqs.py

--- ele_lgqs.py
+++ ele_lgqs.py
@@ -167,7 +167,7 @@ class TYT:
 
         try:
             r = requests.post(
-                "http://bj.frp.one:17151/getXSign",
+                "http://192.168.123.4:17152/api/getXSign",
                 json=body
             )
             r.raise_for_status()
