2dcbc2031b60e94453ea4837a5132c6d18ae6750
zhpphz <121403179+zhpphz@users.noreply.github.com>
2024-09-14 12:34:46 +0800

    Update ele_femf.js

--- ele_femf.js
+++ ele_femf.js
@@ -1,7 +1,7 @@
-/**
- * 变量：elmck: 必填，账号cookie
- * cron: 25 */4 * * *
- */
+// /**
+//  * 变量：elmck: 必填，账号cookie
+//  * cron: 25 */4 * * *
+//  */
 
 const $ = new Env('饿了么福尔魔方');
 const qs = require("qs");
